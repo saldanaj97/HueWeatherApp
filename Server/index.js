71fd51d24224c9cc4b0d2e93f17f71fd7dfc3ae2
Juan Saldana <saldanaj97@gmail.com>
2021-08-05 21:16:41 -0500

    Changed variable names and cleaned unused code

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const https = require("https");
+const axios = require("axios");
 require("dotenv").config();
 
-// Basic express boilerplate code
+// Variable to hold the current weather conditions
+var currentConditions = "none";
+
 app.use(express.json());
-app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -18,12 +19,7 @@ app.listen(port, () => {
   );
 });
 
-// Open weather API
-const weather = require("openweather-apis");
-const axios = require("axios");
-var conditions = "none";
-
-axios({
+var getWeatherRequest = axios({
   method: "GET",
   url: "https://api.openweathermap.org/data/2.5/weather",
   headers: {
@@ -38,8 +34,12 @@ axios({
     console.log(
       `Current weather conditions from the API: ${response.data.weather[0].main}`
     );
-    conditions = response.data.weather[0].main;
+    currentConditions = response.data.weather[0].main;
   })
   .catch((error) => {
     console.log(error);
   });
+
+app.get("/conditions", (getWeatherRequest, res) => {
+  res.send(`Current weather conditions from the API: ${currentConditions}. `);
+});
