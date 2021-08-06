37c0f51112c36593727d4e83617524499f95139e
Juan Saldana <saldanaj97@gmail.com>
2021-08-05 19:53:08 -0500

    Basic calls to weather API are working

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+require("dotenv").config();
 
 // Basic express boilerplate code
 app.get("/", (req, res) => {
@@ -9,4 +10,19 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// Open weather API
+const weather = require("openweather-apis");
+
+weather.setLang("en");
+weather.setZipCode(75149);
+weather.setUnits("metric");
+weather.setAPPID(process.env.OPEN_WEATHER_API_KEY);
+
+weather.getDescription(function (err, desc) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(desc);
 });
