03840792ac9e9da64eeffe70fc903f3d6228e1f3
Juan Saldana <saldanaj97@gmail.com>
2021-08-06 22:00:23 -0500

    Basic swift app

---
+++ Mobile/Mobile/MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileApp.swift
+//  Mobile
+//
+//  Created by Juan Saldana on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
