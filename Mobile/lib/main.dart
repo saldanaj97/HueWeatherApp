88df0b4aef56db2a1d6a36cf05e9c252c1ef1e43
Juan Saldana <saldanaj97@gmail.com>
2021-08-10 16:37:05 -0500

    Users location is now available through mobile app

--- Mobile/lib/main.dart
+++ Mobile/lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:location/location.dart';
+import 'package:app_settings/app_settings.dart';
+import 'dart:io';
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -13,21 +18,88 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Weather Light Sync'),
+      home: WeatherData(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+class WeatherData extends StatefulWidget {
+  @override
+  _WeatherDataState createState() => _WeatherDataState();
+}
 
-  final String title;
+class _WeatherDataState extends State<WeatherData> {
+  String longitude = '';
+  String latitude = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.granted) {
+      print('Location Permissions Granted');
+    } else if (_permissionGranted != PermissionStatus.granted) {
+      _permissionGranted = await location.requestPermission();
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+          title: Text('Location Permission'),
+          content: Text('This app needs location access to set the lights according to the weather. '),
+          actions: <Widget>[
+            CupertinoDialogAction(
+              child: Text('Deny'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            CupertinoDialogAction(
+              child: Text('Settings'),
+              onPressed: () => AppSettings.openAppSettings(),
+            ),
+          ],
+        ),
+      );
+    }
+    _locationData = await location.getLocation();
+    setState(() {
+      _locationData.longitude.toString() != '' ? longitude = _locationData.longitude.toString() : longitude = 'null';
+      _locationData.latitude.toString() != '' ? latitude = _locationData.latitude.toString() : latitude = 'null';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: title,
-      home: Scaffold(appBar: AppBar(title: const Text('Weather Light Sync')), body: const Center(child: Text('Center Text'))),
+      title: 'Weather Light Sync',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Weather Light Sync')),
+        body: Container(
+          alignment: Alignment.center,
+          child: ElevatedButton(
+            onPressed: () => {
+              //getUserAPI(),
+            },
+            child: Text('Get weather data'),
+          ),
+        ),
+      ),
     );
   }
 }
