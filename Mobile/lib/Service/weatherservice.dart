4ae0af2419262fd97a4d96d33b7a328cd3a84a08
Juan Saldana <saldanaj97@gmail.com>
2021-08-25 14:13:50 -0500

    Removed extra print statement

--- Mobile/lib/Service/weatherservice.dart
+++ Mobile/lib/Service/weatherservice.dart
@@ -3,7 +3,6 @@ import 'package:mobile/Model/location.dart';
 
 class WeatherService {
   Future<http.Response> getCurrentWeather(LocationCoordinates coordinates) async {
-    print(' longitude: ' + coordinates.longitude);
     // Call the API for the weather and save the response
     return await http.get(Uri.parse('http://localhost:3000/conditions/${coordinates.latitude}/${coordinates.longitude}'));
   }
