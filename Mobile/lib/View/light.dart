b32ca8d4a82c58db85ef057c7099daa34250c0ba
Juan Saldana <saldanaj97@gmail.com>
2021-09-03 14:58:16 -0500

    Light list now updates to current color of light

--- Mobile/lib/View/light.dart
+++ Mobile/lib/View/light.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'dart:convert';
+import 'dart:math';
 import 'package:mobile/Model/light.dart';
 import 'package:mobile/Service/lightservice.dart';
 
@@ -99,12 +100,14 @@ class _LightsViewState extends State<LightsView> {
 
   // Widget for each individual light container in the list
   Widget lightList(_lights, index) {
+    List rgb = XYtoRGB(_lights[index].lightState.x, _lights[index].lightState.y, _lights[index].lightState.bri.toDouble() / 254);
+    //print(rgb);
     return ClipRRect(
       borderRadius: BorderRadius.circular(15.0),
       child: Container(
         width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height / 13,
-        color: CupertinoColors.darkBackgroundGray,
+        height: MediaQuery.of(context).size.height / 13, // TODO: Fix this height to be cleaner
+        color: Color.fromRGBO(rgb[0].round() * 255, rgb[1].round() * 255, rgb[2].round() * 255, _lights[index].lightState.bri.toDouble() / 254),
         child: Row(
           children: [
             Row(
@@ -115,7 +118,7 @@ class _LightsViewState extends State<LightsView> {
                   child: Text(
                     _lights[index].lightInfo.name,
                     textAlign: TextAlign.left,
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                   ),
                 ),
                 Container(
@@ -140,4 +143,29 @@ class _LightsViewState extends State<LightsView> {
 // TODO: Implement a function to convert the hue XY array to RGB for the application to use
 // Formulas for hue color conversion are from the link below:
 // https://github.com/PhilipsHue/PhilipsHueSDK-iOS-OSX/commit/00187a3db88dedd640f5ddfa8a474458dff4e1db
+
+  List XYtoRGB(double hueX, double hueY, double brightness) {
+    List rgb = [];
+
+    // Calculate xyz values
+    double x = hueX;
+    double y = hueY;
+    double z = 1.0 - x - y;
+    double _y = brightness;
+    double _x = (_y / y) * x;
+    double _z = (_y / y) * z;
+
+    // Convert to rgb
+    double r = _x * 1.4628067 - _y * 0.1840623 - _z * 0.2743606;
+    double g = -_x * 0.5217933 + _y * 1.4472381 + _z * 0.0677227;
+    double b = (_x * 0.0349342 - _y * 0.0968930 + _z * 1.2884099);
+
+    // Add to the list and return the list
+    rgb.add(r);
+    rgb.add(g);
+    rgb.add(b);
+
+    print(rgb);
+    return rgb;
+  }
 }
