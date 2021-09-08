14e2f16d7fb941c7d7e018efbae5ad406a6d2110
Juan Saldana <saldanaj97@gmail.com>
2021-09-08 17:26:52 -0500

    Tweaked the colors

--- Mobile/lib/View/light.dart
+++ Mobile/lib/View/light.dart
@@ -119,11 +119,15 @@ class _LightsViewState extends State<LightsView> {
             color: Color.fromRGBO(44, 45, 64, 1),
             shape: BoxShape.rectangle,
             borderRadius: BorderRadius.circular(6.0),
+            gradient: LinearGradient(begin: Alignment(1, 3), end: Alignment(.3, 0), colors: [
+              Color.fromRGBO(26, 27, 38, 1).withOpacity(.3),
+              Color.fromRGBO(35, 35, 50, 1).withOpacity(0.2),
+            ]),
             boxShadow: [
-              BoxShadow(color: Color.fromRGBO(35, 36, 51, 1), spreadRadius: 0.0, blurRadius: 15, offset: Offset(3.0, 3.0)),
-              BoxShadow(color: Color.fromRGBO(26, 27, 38, 1), spreadRadius: 0.0, blurRadius: 15 / 2.0, offset: Offset(3.0, 3.0)),
-              BoxShadow(color: Color.fromRGBO(50, 50, 70, 1), spreadRadius: 2, blurRadius: 10, offset: Offset(-3.0, -3.0)),
-              BoxShadow(color: Color.fromRGBO(50, 50, 70, 1), spreadRadius: 2, blurRadius: 10 / 2, offset: Offset(-3.0, -3.0)),
+              BoxShadow(color: Color.fromRGBO(35, 36, 51, 1), spreadRadius: 0.0, blurRadius: 10, offset: Offset(3.0, 3.0)),
+              BoxShadow(color: Color.fromRGBO(26, 27, 38, 1), spreadRadius: 0.0, blurRadius: 10 / 2.0, offset: Offset(3.0, 3.0)),
+              BoxShadow(color: Color.fromRGBO(50, 50, 70, 1), spreadRadius: 2.0, blurRadius: 5, offset: Offset(-3.0, -3.0)),
+              BoxShadow(color: Color.fromRGBO(50, 50, 70, 1), spreadRadius: 2.0, blurRadius: 5 / 2, offset: Offset(-3.0, -3.0)),
             ],
           ),
           child: Row(
