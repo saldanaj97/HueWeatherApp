265e832c9d6cadad432d8e07201344f4d6620776
Juan Saldana <saldanaj97@gmail.com>
2021-09-21 14:44:32 -0500

    Moved widgets into their appropriate files

--- Mobile/lib/View/skeleton_screens/weatherSkeleton.dart
+++ Mobile/lib/View/skeleton_screens/weatherSkeleton.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:skeleton_text/skeleton_text.dart';
 import 'package:mobile/View/decorations/decorations.dart';
-import 'package:mobile/View/widgets/skeleton_loading_widgets.dart';
+import 'package:mobile/View/skeleton_screens/lightSkeleton.dart';
 import 'package:mobile/View/widgets/navbar.dart';
 
 class WeatherSkeletonLoading extends StatelessWidget {
@@ -46,5 +48,99 @@ class WeatherSkeletonLoading extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  // This will be the skeleton loading widget for the weather card on the weather screen
+  Widget weatherCardSkeleton(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width / 1.15,
+              margin: EdgeInsets.only(top: 10, bottom: 35),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6.0),
+              ),
+              child: Neumorphic(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(top: 15),
+                        margin: EdgeInsets.only(bottom: 15),
+                        child: SkeletonAnimation(
+                          borderRadius: BorderRadius.circular(25.0),
+                          child: Container(
+                            height: 45,
+                            width: MediaQuery.of(context).size.width * 0.75,
+                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(25.0), color: primaryColor),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: SkeletonAnimation(
+                          borderRadius: BorderRadius.circular(100.0),
+                          child: Container(
+                            height: 120,
+                            width: 120,
+                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(25.0), color: primaryColor),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 15),
+                        child: SkeletonAnimation(
+                          borderRadius: BorderRadius.circular(25.0),
+                          child: Container(
+                            height: 45,
+                            width: MediaQuery.of(context).size.width * 0.55,
+                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(25.0), color: primaryColor),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 15),
+                        child: SkeletonAnimation(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: Container(
+                            height: 75,
+                            width: MediaQuery.of(context).size.width * 0.2,
+                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(15.0), color: primaryColor),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 15),
+                        child: SkeletonAnimation(
+                          borderRadius: BorderRadius.circular(25.0),
+                          child: Container(
+                            height: 25,
+                            width: MediaQuery.of(context).size.width * 0.6,
+                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(25.0), color: primaryColor),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 15),
+                        margin: EdgeInsets.only(bottom: 20),
+                        child: SkeletonAnimation(
+                          borderRadius: BorderRadius.circular(25.0),
+                          child: Container(
+                            height: 25,
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(25.0), color: primaryColor),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  style: neumorphicBox),
+            ),
+          ],
+        ),
+      )
+    ]);
   }
 }
